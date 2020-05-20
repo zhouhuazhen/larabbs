aa679e6d9217e843bd3eab2d86faa280944e8cf3
zhouhuazhen <382938018@qq.com>
2020-05-20 11:05:59 +0800

    生成用户认证代码

--- config/app.php
+++ config/app.php
@@ -81,6 +81,7 @@ return [
     */
 
     'locale' => 'zh-CN',
+    // 'locale' => 'en_US',
 
     /*
     |--------------------------------------------------------------------------
@@ -158,7 +159,8 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
+        // Illuminate\Translation\TranslationServiceProvider::class,
+        Overtrue\LaravelLang\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
